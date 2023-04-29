78bf1a8809918188bc886035cc467351a6c20f50
anwargithub08 <playmakeranwar08@gmail.com>
2023-04-29 16:08:19 +0700

    kantor/proses membuat about me

--- resources/views/livewire/about-me-component.blade.php
+++ resources/views/livewire/about-me-component.blade.php
@@ -2,7 +2,20 @@
     <div class="card card-about-me">
         <div class="card-header"><strong>Tentang Admin & SIstem Ini</strong></div>
         <div class="card-body">
-            afgagfa
+            <p><strong>Khoirul Anwar</strong></p>
+            <p><strong>Rembang, ** Mei 1999</strong></p>
+            <p><strong>Laki - Laki</strong></p>
+            <p><strong><a href="https://goo.gl/maps/F9sSDwhkn5nNdKtFA" target="_blank"
+                        onclick="return confirm('cek maps..?')">Bogorejo, 02/01, Sumber, Rembang, Jawa
+                        Tengah, 59253</a></strong></p>
+            <hr>
+            <p>Sistem ini dibuat karena admin pernah berjualan di marketlace dan banyak pembeli yang bertransaksi,
+                setelah beberapa transaksi berhasil diselesaikan, admin mulai dikenakan pajak dari pihak marketplace,
+                jadi admin harus menaikkan harga produk.</p>
+            <p>Ketika harga produk dianaikkan, pembeli jarang yang bertransaksi</p>
+            <p>Itulah alasan sistem ini dibuat</p>
+            <p>Kekuarangan dari sistem ini, metode pembayaran hanya melalui BANK BRI & QRIS, jika transfer sesama BANK
+                BRI maka tidak ada biaya admin, dan jika menggunakan QRIS juga tidak ada biaya admin</p>
         </div>
     </div>
 </div>
